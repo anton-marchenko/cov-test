3e478743f49cc0e178e83b9779f4d93ada3b918f
anton-marchenko <fattan@ya.ru>
2020-02-07 14:46:02 +0300

    t3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cov-test 2
+
+[![Coverage Status](https://coveralls.io/repos/github/anton-marchenko/cov-test/badge.svg)](https://coveralls.io/github/anton-marchenko/cov-test)
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function test (val) {
+    return val + 1;
+}
+
 exports.add = function () {
+    const vv = test() +1;
+    if (vv === 100500) {
+        return 1;
+    }
     return 2 + 2;
 };
